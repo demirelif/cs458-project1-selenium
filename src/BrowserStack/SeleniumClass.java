685074291f40f94329fcd5a1b59c2198866d826b
elif.demir <ed.demirelif@gmail.com>
2021-02-11 23:54:18 +0300

    Initial commit

---
+++ src/BrowserStack/SeleniumClass.java
@@ -0,0 +1,4 @@
+package BrowserStack;
+
+public class SeleniumClass {
+}
---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+    public static void main(String[] args) {
+// TODO Auto-generated method stub
+
+//setting the driver executable
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+
+//Initiating your chromedriver
+        WebDriver driver=new ChromeDriver();
+
+//Applied wait time
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//maximize window
+        driver.manage().window().maximize();
+
+//open browser with desried URL
+        driver.get("https://www.google.com");
+
+//closing the browser
+        driver.close();
+
+    }
+
+}
